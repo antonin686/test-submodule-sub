7bf9c3cc1da63d5db3e7f0217be235bf322e7ad0
Antonin Islam <antonin.islam@brainstation-23.com>
2025-01-15 15:12:15 +0600

    adds four starts

--- characters/four_stars.ts
+++ characters/four_stars.ts
@@ -1,6 +1,10 @@
 export const characters = [
-    { name: 'Eve', rarity: 4 },
-    { name: 'Finn', rarity: 4 },
-    { name: 'Gwen', rarity: 4 },
-    { name: 'Hans', rarity: 4 },
-  ];
+    { name: "Eve", rarity: 4 },
+    { name: "Finn", rarity: 4 },
+    { name: "Gwen", rarity: 4 },
+    { name: "Hans", rarity: 4 },
+    { name: "Ivan", rarity: 4 },
+    { name: "Jenny", rarity: 4 },
+    { name: "Kevin", rarity: 4 },
+    { name: "Lily", rarity: 4 },
+];
